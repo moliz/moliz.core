658be6bf0e35128ecfedfbeee5bad4442fc7f971
Tanja Mayerhofer <mayerhofer@big.tuwien.ac.at>
2017-04-10 11:04:38 +0200

    Removes non core projects

--- org.modelexecution.xmof.examples.petrinet.diagram/src/petrinet/diagram/edit/helpers/TransitionEditHelper.java
+++
@@ -1,7 +0,0 @@
-package petrinet.diagram.edit.helpers;
-
-/**
- * @generated
- */
-public class TransitionEditHelper extends PetrinetBaseEditHelper {
-}
--- org.modelexecution.xmof.examples.petrinet2.diagram/src/petrinet2/diagram/edit/helpers/NetEditHelper.java
+++
@@ -1,7 +0,0 @@
-package petrinet2.diagram.edit.helpers;
-
-/**
- * @generated
- */
-public class NetEditHelper extends Petrinet2BaseEditHelper {
-}
