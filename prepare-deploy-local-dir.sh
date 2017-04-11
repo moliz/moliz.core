2f6f60a670388f19601f548c4bf92b950046256a
dvojtise <didier.vojtisek@inria.fr>
2017-04-11 10:18:37 +0200

    moving nightly update site to moliz.core gh-pages

--- prepare-deploy-local-dir.sh
+++ prepare-deploy-local-dir.sh
@@ -5,7 +5,7 @@ echo "Prepare deploy local dir = ${DEPLOY_LOCAL_DIR}"
 # Create nightly folder
 mkdir -p $DEPLOY_LOCAL_DIR 
 # Copy update-site to deploy local dir
-cp -r $UPDATE_SITE_DIR/repository $DEPLOY_LOCAL_DIR
+cp -r $UPDATE_SITE_DIR/repository/* $DEPLOY_LOCAL_DIR
 cp -r $UPDATE_SITE_DIR/*.zip $DEPLOY_LOCAL_DIR
 echo "ls ${DEPLOY_LOCAL_DIR}"
 ls $DEPLOY_LOCAL_DIR
